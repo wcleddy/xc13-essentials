580c83ad295830798c416e3c24a1c82d72ef6852
Bill Leddy <wleddy@calpoly.edu>
2023-03-15 10:11:50 -0700

    Initial Commit

---
+++ SourceControl2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SourceControl2
+//
+//  Created by Bill Leddy on 3/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SourceControl2/SourceControl2App.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControl2App.swift
+//  SourceControl2
+//
+//  Created by Bill Leddy on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct SourceControl2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
